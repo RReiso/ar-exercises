6c3ca4c84eacdf46624b4796dbef3d1310b79b64
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-31 14:58:07 +0000

    Finish exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,15 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
+# * Richmond (annual_revenue of 1260000 carries women's apparel only)
+#   * Gastown (annual_revenue of 190000 carries men's apparel only)
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
+
+
 # Your code goes below here ...
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
